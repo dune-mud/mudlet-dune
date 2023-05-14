2b89953ea45a21574ec87d317ff1b62541c9a210
Daniel McCarney <daniel@binaryparadox.net>
2023-05-13 21:23:34 -0400

    resources: add minimal MDK dependencies.
    Proof of concept for:
    
    * Loading MDK dependency.
    * Requiring EMCO into the plugin script.
    
    Many thanks to demonnic for the A+++ tooling, library and help :)

--- src/scripts/DuneMUD/Code.lua
+++ src/scripts/DuneMUD/Code.lua
@@ -3,3 +3,7 @@ dunemud = dunemud or {}
 dunemud.config = dunemud.config or defaultConfig
 
 cecho("<green>Loaded DuneMUD Mudlet<reset>\n")
+
+local EMCO = require("@PKGNAME@.mdk.emco")
+
+cecho("<green>Loaded EMCO<reset>\n")
