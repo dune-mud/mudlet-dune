039c067c219e1f3329d65227479f6e0d14a0aa52
Daniel McCarney <daniel@binaryparadox.net>
2023-05-21 17:24:03 -0400

    mapper: comment out debug prints for now

--- src/scripts/mapper/mapper.lua
+++ src/scripts/mapper/mapper.lua
@@ -39,28 +39,28 @@ local function configure_room()
   local info = map.room_info
   local coords = {0,0,0}
 
-  echo("Setting room ".. info.vnum .." to name ".. info.name .."\n")
+  --echo("Setting room ".. info.vnum .." to name ".. info.name .."\n")
   setRoomName(info.vnum, info.name)
 
   local areas = getAreaTable()
   local areaID = areas[info.area]
   if not areaID then
-    echo("adding new area ID for ".. info.area .."\n")
+    --echo("adding new area ID for ".. info.area .."\n")
     areaID = addAreaName(info.area)
   else
-    echo("using existing area ID ".. areaID .." for area ".. info.area .."\n")
+    --echo("using existing area ID ".. areaID .." for area ".. info.area .."\n")
   end
   setRoomArea(info.vnum, areaID)
 
-  echo("Setting initial coords\n")
+  --echo("Setting initial coords\n")
 
   local coords = {0,0,0}
   if map.prev_info.vnum and getRoomCoordinates(map.prev_info.vnum) then
-    echo("Setting initial coords to previous room's coords\n")
+    --echo("Setting initial coords to previous room's coords\n")
     coords = {getRoomCoordinates(map.prev_info.vnum)}
-    echo("Coords: ".. coords[1] ..", ".. coords[2] ..", ".. coords[3] .."\n")
+    --echo("Coords: ".. coords[1] ..", ".. coords[2] ..", ".. coords[3] .."\n")
   else
-    echo("Using 0,0,0 as initial coords. No prev room coords\n")
+    --echo("Using 0,0,0 as initial coords. No prev room coords\n")
   end
 
   local shift = {0,0,0}
@@ -71,7 +71,7 @@ local function configure_room()
   for dir,roomID in pairs(info.exits) do
     if roomID == map.prev_info.vnum and move_vectors[dir] then
       shift = move_vectors[dir]
-      echo("Found a shift from room ID ".. roomID .. " for dir ".. dir .. "\n")
+      --echo("Found a shift from room ID ".. roomID .. " for dir ".. dir .. "\n")
       break
     end
   end
@@ -80,17 +80,17 @@ local function configure_room()
     coords[n] = (coords[n] or 0) - (shift[n] or 0)
   end
 
-  echo("Considering this room (pre-shift) at ".. coords[1] ..", ".. coords[2] ..", ".. coords[3] .."\n")
+  --echo("Considering this room (pre-shift) at ".. coords[1] ..", ".. coords[2] ..", ".. coords[3] .."\n")
   -- map stretching
   local overlap = getRoomsByPosition(areaID, coords[1],coords[2],coords[3])
   if not table.is_empty(overlap) then
-    echo("Need to stretch - found rooms at the pre-shift position.\n")
+    --echo("Need to stretch - found rooms at the pre-shift position.\n")
     local rooms = getAreaRooms(areaID)
     local rcoords
-    echo("Shift:")
+    --echo("Shift:")
     display(shift)
     for _,id in ipairs(rooms) do
-      echo("Considering room id ".. id .." at ")
+      --echo("Considering room id ".. id .." at ")
       rcoords = {getRoomCoordinates(id)}
       display(rcoords)
       for n = 1,3 do
@@ -98,12 +98,12 @@ local function configure_room()
           rcoords[n] = rcoords[n] - shift[n]
         end
       end
-      echo("Moving to ")
+      --echo("Moving to ")
       display(rcoords)
       setRoomCoordinates(id,rcoords[1],rcoords[2],rcoords[3])
     end
   end
-  echo("Setting post-shift coords: (".. coords[1] .. ", ".. coords[2] .. ", ".. coords[3] ..")\n")
+  --echo("Setting post-shift coords: (".. coords[1] .. ", ".. coords[2] .. ", ".. coords[3] ..")\n")
   setRoomCoordinates(info.vnum, coords[1], coords[2], coords[3])
 
   --if info.exits then
@@ -112,59 +112,59 @@ local function configure_room()
 
   for dir, id in pairs(info.exits) do
     if id and getRoomName(id) ~= "" then
-      echo("Setting exit "..dir.." to room ID ".. id .. "\n")
+      --echo("Setting exit "..dir.." to room ID ".. id .. "\n")
       setExit(info.vnum, id, dir)
     elseif stubmap[dir] then
-      echo("Creating stub exit for normal dir ".. dir .." to stub room ID ".. id .."\n")
+      --echo("Creating stub exit for normal dir ".. dir .." to stub room ID ".. id .."\n")
       setExitStub(info.vnum, dir, true)
     else
-      echo("(TBD) Creating special exit for dir ".. dir .." to stub room ID ".. id .."\n")
+      --echo("(TBD) Creating special exit for dir ".. dir .." to stub room ID ".. id .."\n")
     end
   end
 
-  echo("Done configuring room\n")
+  --echo("Done configuring room\n")
 end
 
 local function handle_move()
-  echo("Running handle_move()\n")
+  --echo("Running handle_move()\n")
   local info = map.room_info
-  echo("Current hash ".. info.hash .. "\n")
+  --echo("Current hash ".. info.hash .. "\n")
   local id = getRoomIDbyHash(info.hash)
-  echo("Current ID ".. id .. "\n")
+  --echo("Current ID ".. id .. "\n")
 
   -- If we haven't created a stub room yet, do that now.
   if id == -1 then
     info.vnum = make_stub_room(info.hash)
-    echo("Created stub room with ID ".. info.vnum .. " for this room\n")
+    --echo("Created stub room with ID ".. info.vnum .. " for this room\n")
   else
     info.vnum = id
-    echo("Found existing room with ID ".. info.vnum .. " for this room\n")
+    --echo("Found existing room with ID ".. info.vnum .. " for this room\n")
   end
 
   if not getRoomName(id) or getRoomName(id) == "" then
-    echo("Configuring room\n")
+    --echo("Configuring room\n")
     configure_room()
   else
-    echo("Room was already configured\n")
+    --echo("Room was already configured\n")
   end
 
   local stubs = getExitStubs1(info.vnum)
   if stubs then
-    echo("Processing stubs.\n")
+    --echo("Processing stubs.\n")
     for _, n in ipairs(stubs) do
       dir = stubmap[n]
       local id = info.exits[dir]
       if getRoomName(id) ~= "" then
         setExit(info.vnum, id, dir)
-        echo("setting exit ".. dir .. " to ".. id .. "\n")
+        --echo("setting exit ".. dir .. " to ".. id .. "\n")
       else
-        echo("not setting exit for ".. dir .. "\n")
+        --echo("not setting exit for ".. dir .. "\n")
       end
     end
   else
-    echo("No stubs to process\n")
+    --echo("No stubs to process\n")
   end
-  echo("Centering on ".. info.vnum .."\n")
+  --echo("Centering on ".. info.vnum .."\n")
   centerview(info.vnum)
 end
 
@@ -182,6 +182,10 @@ function DuneMUD.map.tearDown()
   --  local generic_mapper_url = "https://raw.githubusercontent.com/Mudlet/Mudlet/development/src/mudlet-lua/lua/generic-mapper/generic_mapper.xml"
   --  installPackage(generic_mapper_url)
   --end
+end
+
+function doSpeedWalk(...)
+  cecho("Speed walking not implemented yet...\n")
 end
 
 function DuneMUD.map.onRoomInfo(_, ...)
@@ -219,9 +223,9 @@ function DuneMUD.map.onRoomInfo(_, ...)
     id = getRoomIDbyHash(h)
     if id == -1 then
       id = make_stub_room(h)
-      echo("exit ".. dir .." pointing to stub room id ".. id .. "\n")
+      --echo("exit ".. dir .." pointing to stub room id ".. id .. "\n")
     else
-      echo("exit ".. dir .." pointing to configured room id ".. id .. "\n")
+      --echo("exit ".. dir .." pointing to configured room id ".. id .. "\n")
     end
     map.room_info.exits[dir] = id
   end
